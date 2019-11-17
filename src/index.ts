fbce228bcc364354c1bb52ce1e6ea14eb6f90ad3
matheusps <cout.matheusps@gmail.com>
2019-11-17 08:44:00 -0300

    Rename function

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,27 @@ const createTokens = (styles: DraftToken) => {
   const { propName, values } = styles
   const props = Object.keys(propName)
 
-  const styleFunctions = props.map(prop => {
+  const styledFunctions = props.map(prop => {
     const name = propName[prop]
 
-    const curriedStyleFn = (key, values) => receivedProps => ({
-      [prop]: values[receivedProps[key]]
+    const curriedStyleFn = (name: string, values: { [key: string]: any }) => (
+      receivedProps: any
+    ) => ({
+      [prop]: values[receivedProps[name]]
     })
 
     return curriedStyleFn(name, values)
   })
 
-  return styleFunctions
+  return styledFunctions
 }
 
 const createVariation = (draft: DraftVariation) => {
   const { variation, name } = draft
 
-  const styledFunction = (name: string, variationValues: { [key: string]: any}) => (receivedProps: any) => {
+  const styledFunction = (name: string, variationValues: { [key: string]: any }) => (
+    receivedProps: any
+  ) => {
     const passedVariation = receivedProps[name]
     const props = variationValues[passedVariation]
     return props
